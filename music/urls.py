f1283bcac8fd6633d7de253b90f9b40dda4e6f92
Sherdos <sherdos26@gmail.com>
2024-08-13 15:02:57 +0600

    new tracks

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from music.views import index
+from music.views import index,show_news
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('show/news/<int:id>/', show_news, name='show_news'),
+    
 ]
--- music/views.py
+++ music/views.py
@@ -6,6 +6,7 @@ from music.models import Music, News
 def index(request):
     music = Music.objects.latest('id')
     news = News.objects.all().order_by('-id')
+    new_tracks = Music.objects.all().order_by('-date_pub')
     if request.GET.get('track'):
         id = request.GET.get('track')
         music = Music.objects.get(id=id)
@@ -13,7 +14,15 @@ def index(request):
     context = {
         'music':music,
         'musics':musics,
-        'news':news
+        'news':news,
+        'new_tracks':new_tracks,
     }
     return render(request, 'index.html', context)
 
+
+def show_news(request,id):
+    news = News.objects.get(id=id)
+    context = {
+        'news':news
+    }
+    return render(request, 'show_news.html',context)
